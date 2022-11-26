efb05cd68dea53eeaae304fbc9f12133922817f2
pkalashnikov <pavelkalaschnikov@gmail.com>
2022-11-27 00:06:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 const FLOOR = 'FLOOR'
 const EMPTY = 'EMPTY'
+const MINES = 'MINES'
 const BALL = 'BALL'
 const GAMER = 'GAMER'
 const PASSAGE = 'PASSAGE'
-const MINES = 'MINES'
 
 const GAMER_IMG = '<img src="img/gamer.png">'
 const BALL_IMG = '<img src="img/ball.png">'
@@ -23,9 +23,10 @@ var level = 16
 var boardSize = document.querySelector('.main')
 
 function onInitGame(number) {
-    if (number){ 
+    if (number) {
         level = number
-        onInitGame()}
+        onInitGame()
+    }
     //minesCount = 0
     gBoard = buildBoard()
 
@@ -41,37 +42,41 @@ function buildBoard() {
     var b
     var l = 0
     var minesCell = []
-    var mineCell = []
-    for (var i = 0; minesCell.length<=mines; i++) {
+    for (var i = 0; minesCell.length < mines; i++) {
         a = getRandomInt(0, Math.sqrt(level))
-        mineCell[a]=[]
         b = getRandomInt(0, Math.sqrt(level))
-        //const minecell = minesCell[a][b]
-       
-         minesCell.push({ a, b })
-
+        if (inArray([a, b], minesCell) === false) {
+            minesCell.push([a, b])
+            console.log([a, b])
+        }
     }
-    console.log(minesCell)
-   
+
+
     for (var i = 0; i < Math.sqrt(level); i++) {
         board[i] = []
         for (var j = 0; j < Math.sqrt(level); j++) {
-            board[i][j] = { type: EMPTY, gameElement: null }
-            
+            board[i][j] = { type: FLOOR, gameElement: null }
+
             // board[i][j] = { type: FLOOR, gameElement: null }
             // board[i][j] = { type: MINES, gameElement: null }
             // board[i][j] = { type: FLAG, gameElement: null }
-        
+        }
     }
-}
-while (l < mines) {
-    var c = minesCell[l].a
-    var d = minesCell[l].b
-    l++
-    console.log(c,d)
-    board[c][d] = { type: MINES, gameElement: null }
-}
+    while (l < mines) {
+        var c = minesCell[l][0]
+        var d = minesCell[l][1]
+        l++
+        // console.log(c,d)
+        board[c][d] = { type: MINES, gameElement: null }
+    }
     return board
+}
+function inArray(needle, haystack) {
+    var length = haystack.length;
+    for (var i = 0; i < length; i++) {
+        if (haystack[i][0] === needle[0] && haystack[i][1] === needle[1]) return true;
+    }
+    return false;
 }
 
 // Render the board to an HTML table
@@ -103,19 +108,34 @@ function renderBoard(board) {
     const elBoard = document.querySelector('.divTableRow')
     var strHTML = ''
     for (var i = 0; i < Math.sqrt(level); i++) {
-        strHTML += '<tr>\n'
         for (var j = 0; j < Math.sqrt(level); j++) {
             const currCell = board[i][j]
 
             var cellClass
 
-            if (currCell.type === EMPTY) {
-                cellClass += ' divTableCell-empty'
-                strHTML += `\t<div class="divTableCell-empty"></div>\n`
+            if (currCell.type === FLOOR) {
+                cellClass += ' divTableCell'
+                if (countMinesAround(board, i, j) === 1) {
+                    strHTML += `\t<div class="divTableCell"><span style="color:#3016B0">${countMinesAround(board, i, j)}</span></div>\n`
+                }
+                else if (countMinesAround(board, i, j) === 2) {
+                    strHTML += `\t<div class="divTableCell"><span style="color:#22884F">${countMinesAround(board, i, j)}</span></div>\n`
+                }
+                else if (countMinesAround(board, i, j) === 3) {
+                    strHTML += `\t<div class="divTableCell"><span style="color:#BF3030">${countMinesAround(board, i, j)}</span></div>\n`
+                }
+                else if (countMinesAround(board, i, j) === 4) {
+                    strHTML += `\t<div class="divTableCell"><span style="color:#06266F">${countMinesAround(board, i, j)}</span></div>\n`
+                }
+                else if (countMinesAround(board, i, j) === 5) {
+                    strHTML += `\t<div class="divTableCell"><span style="color:#85004B">${countMinesAround(board, i, j)}</span></div>\n`
+                }
+
+                else { strHTML += `\t<div class="divTableCell"><span></span></div>\n` }
             }
             else if (currCell.type === MINES) {
-                cellClass += ' divTableCell'
-                strHTML += `\t<div class="mine"><span></span></div>\n`
+                cellClass += ' mine'
+                strHTML += `\t<div class="mine"></div>\n`
             }
         }
     }
@@ -123,24 +143,7 @@ function renderBoard(board) {
     countMinesAround(gBoard, i, j)
 }
 
-// Move the player to a specific location
-// Convert a location object {i, j} to a selector and render a value in that element
-// function renderCell(location, value) {
-//     const cellSelector = '.' + getClassName(location) // cell-i-j
-//     const elCell = document.querySelector(cellSelector)
-//     elCell.innerHTML = value
 
-// }
-function getmines() {
-    var minesCell = []
-
-    for (var i = 0; i < mines; i++) {
-        const a = getRandomInt(0, Math.sqrt(level))
-        const b = getRandomInt(0, Math.sqrt(level))
-        minesCell.push({ a, b })
-    }
-    return minesCell
-}
 function getRandomInt(min, max) {
     var min = Math.ceil(min);
     var max = Math.floor(max);
@@ -159,35 +162,6 @@ function gameOver() {
     elGameOver.innerHTML = strHTML
 
 }
-
-
-
-// function getEmptyCell(board) {
-//     var emptyCells = []
-//     for (var i = 0; i < Math.sqrt(level); i++) {
-//         for (var j = 0; j < Math.sqrt(level); j++) {
-
-//             if (gBoard.gameElement === null && board.type !== FLOOR) {
-//                 emptyCells.push({ i, j })
-//             }
-//         }
-//     }
-
-//     // if (emptyCells.length === 0) gameOver()
-
-//     var randCell = emptyCells[getRandomInt(0, emptyCells.length)]
-
-//     //UPDATE MODEL & DOM
-//     //current cell
-//     // mines++
-//    // gBoard[randCell.i][randCell.j].gameElement = BALL
-//     // renderCell(randCell, BALL_IMG)
-//     //next cell
-
-
-// }
-
-
 function countMinesAround(board, rowIdx, colIdx) {
     var count = 0
     for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
@@ -196,8 +170,9 @@ function countMinesAround(board, rowIdx, colIdx) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= board[0].length) continue
             var currCell = board[i][j]
-            if (currCell === BALL) count++
+            if (currCell.type === MINES) count++
         }
     }
     gNeighborsCount = count
+    return count
 }
